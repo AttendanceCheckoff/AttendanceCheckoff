45fd37db88c62f6f70293a18f75062df044f602d
Sebastian A. Harner <harnersa@rose-hulman.edu>
2019-01-11 12:28:22 -0500

    Added basic activity for single club

--- app/src/main/java/edu/rosehulman/attendancecheckoff/model/Club.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/model/Club.kt
@@ -1,5 +1,34 @@
 package edu.rosehulman.attendancecheckoff.model
 
-data class Club(val name: String, val members: ArrayList<User>, val events: ArrayList<Event>, val officials: Map<User, String>) {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Club(val name: String, val members: ArrayList<User>, val events: ArrayList<Event>, val officials: Map<User, String>) :
+    Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        TODO("members"),
+        TODO("events"),
+        TODO("officials")
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeParcelable(members)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Club> {
+        override fun createFromParcel(parcel: Parcel): Club {
+            return Club(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Club?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
--- app/src/main/java/edu/rosehulman/attendancecheckoff/model/User.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/model/User.kt
@@ -1,5 +1,8 @@
 package edu.rosehulman.attendancecheckoff.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class User(
     val username: String,
     val name: String,
@@ -7,6 +10,37 @@ data class User(
     val year: String,
     val clubs: ArrayList<Club>,
     val events: ArrayList<Event>
-) {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readArrayList(),
+        TODO("events")
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(username)
+        parcel.writeString(name)
+        parcel.writeString(major)
+        parcel.writeString(year)
+        parcel.writeList(clubs)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }

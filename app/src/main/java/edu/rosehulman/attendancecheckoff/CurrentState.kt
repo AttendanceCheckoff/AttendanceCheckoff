55b3df2f7b314d8e90b80812d949c83e276772d0
Sebastian A. Harner <harnersa@rose-hulman.edu>
2019-01-13 17:57:28 -0500

    Adds QR code

---
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/CurrentState.kt
@@ -0,0 +1,8 @@
+package edu.rosehulman.attendancecheckoff
+
+import edu.rosehulman.attendancecheckoff.model.User
+import edu.rosehulman.attendancecheckoff.util.Utils
+
+object CurrentState {
+    var user: User = Utils.testUser
+}
--- app/src/main/java/edu/rosehulman/attendancecheckoff/IDFragment.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/IDFragment.kt
@@ -1,31 +1,26 @@
 package edu.rosehulman.attendancecheckoff
 
-
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import kotlinx.android.synthetic.main.id_fragment.view.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class IDFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.id_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.id_fragment, container, false).apply {
+            image_qr_code.setImageBitmap(generateQRCode())
+        }
     }
 
-
+    private fun generateQRCode(): Bitmap? {
+        val matrix = MultiFormatWriter().encode(CurrentState.user.username, BarcodeFormat.QR_CODE, 600, 600)
+        return BarcodeEncoder().createBitmap(matrix)
+    }
 }

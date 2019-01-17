b553ccd63b174bd7695630989f1233dd1cc37239
Sebastian A. Harner <harnersa@rose-hulman.edu>
2019-01-17 16:55:14 -0500

    Restructures models

--- app/src/main/java/edu/rosehulman/attendancecheckoff/model/Event.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/model/Event.kt
@@ -2,26 +2,37 @@ package edu.rosehulman.attendancecheckoff.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.google.firebase.firestore.Exclude
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class Event(
     val name: String,
     val dateTime: Date,
-    val attendedMembers: ArrayList<User>,
-    val description: String) : Parcelable {
+    val description: String,
+    val attendedMembers: ArrayList<String> = ArrayList(),
+    val clubId: Long
+    ) : Parcelable {
+
+    @get:Exclude var id = ""
 
     constructor(parcel: Parcel) : this(
         parcel.readString(),
         Date(parcel.readLong()),
-        parcel.readArrayList(User::class.java.classLoader) as ArrayList<User>,
-        parcel.readString()
-    )
+        parcel.readString(),
+        parcel.readArrayList(String::class.java.classLoader) as ArrayList<String>,
+        parcel.readLong()
+    ) {
+        this.id = parcel.readString()
+    }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeLong(dateTime.time)
-        parcel.writeList(attendedMembers)
         parcel.writeString(description)
+        parcel.writeList(attendedMembers)
+        parcel.writeLong(clubId)
+        parcel.writeString(id)
     }
 
     override fun describeContents() = 0
@@ -31,4 +42,5 @@ data class Event(
 
         override fun newArray(size: Int): Array<Event?> = arrayOfNulls(size)
     }
+
 }
---
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/model/Official.kt
@@ -0,0 +1,38 @@
+package edu.rosehulman.attendancecheckoff.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.firebase.firestore.Exclude
+
+data class Official(val clubId: String, val userId: String, val role: String, val rank: Int) : Parcelable {
+
+    @get:Exclude
+    var id = ""
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt()
+    ) {
+        id = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(clubId)
+        parcel.writeString(userId)
+        parcel.writeString(role)
+        parcel.writeInt(rank)
+        parcel.writeString(id)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Official> {
+        override fun createFromParcel(parcel: Parcel) = Official(parcel)
+
+        override fun newArray(size: Int): Array<Official?> = arrayOfNulls(size)
+    }
+
+
+}

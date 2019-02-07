19448d37175c0d0c417b82f4c5900ddd9c7e8ec9
Arjun Mahajan <arjunmahajan@Arjun.local>
2019-02-07 08:34:47 +0530

    personal page edited

--- app/src/main/java/edu/rosehulman/attendancecheckoff/club/personal/PersonalAdapter.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/club/personal/PersonalAdapter.kt
@@ -3,16 +3,19 @@ package edu.rosehulman.attendancecheckoff.club.personal
 import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import edu.rosehulman.attendancecheckoff.CurrentState
 import edu.rosehulman.attendancecheckoff.R
 import edu.rosehulman.attendancecheckoff.event.EventActivity
 import edu.rosehulman.attendancecheckoff.model.Club
 import edu.rosehulman.attendancecheckoff.model.Event
 import edu.rosehulman.attendancecheckoff.util.Constants
 
-class PersonalAdapter(val context: Context, val club: Club): RecyclerView.Adapter<PersonalHolder>() {
+class PersonalAdapter(val context: Context?, val club: Club): RecyclerView.Adapter<PersonalHolder>() {
 
     val eventsRef = FirebaseFirestore.getInstance().collection("events")
     val events = ArrayList<Event>()
@@ -35,5 +38,28 @@ class PersonalAdapter(val context: Context, val club: Club): RecyclerView.Adapte
             putExtra(Constants.ARG_INTENT_EVENT, events[position])
         }
         context?.startActivity(intent)
+    }
+
+    fun addSnapshotListener(){
+        eventsRef
+            .whereArrayContains(Event.KEY_ATTENDED_MEMBERS, CurrentState.user.id)
+            .addSnapshotListener { snapshot, firestoreException ->
+                if (firestoreException != null) {
+                    Log.d(Constants.TAG, "Error: $firestoreException")
+                    return@addSnapshotListener
+                }
+                populateLocalEvents(snapshot)
+            }
+
+    }
+
+    fun populateLocalEvents(snapshot: QuerySnapshot?){
+        events.clear()
+        for (document in snapshot!!){
+            val doc = Event.fromSnapshot(document)
+            if (doc.clubId.equals(club.id)){
+                events.add(doc)
+            }
+        }
     }
 }
--- app/src/main/java/edu/rosehulman/attendancecheckoff/club/personal/PersonalFragment.kt
+++ app/src/main/java/edu/rosehulman/attendancecheckoff/club/personal/PersonalFragment.kt
@@ -2,17 +2,21 @@ package edu.rosehulman.attendancecheckoff.club.personal
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import edu.rosehulman.attendancecheckoff.R
 import edu.rosehulman.attendancecheckoff.model.Club
+import kotlinx.android.synthetic.main.fragment_recycler_view.view.*
 
 const val ARG_CLUB = "ARG_CLUB"
 
 class PersonalFragment : Fragment() {
 
     lateinit var club: Club
+    lateinit var adapter: PersonalAdapter
 
     override fun onCreate (savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -23,7 +27,10 @@ class PersonalFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_recycler_view, container, false)
-
+        adapter = PersonalAdapter(context, club)
+        adapter.addSnapshotListener()
+        view.recycler_view.layoutManager = LinearLayoutManager(context)
+        view.recycler_view.adapter = adapter
         return view
     }
 
